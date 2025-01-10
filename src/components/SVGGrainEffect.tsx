b792b858de7d34a08549f144bcde5fdfe083db80
Samshh <samdacs2@gmail.com>
2025-01-11 04:57:28 +0800

    grain change

--- src/components/SVGGrainEffect.tsx
+++ src/components/SVGGrainEffect.tsx
@@ -7,7 +7,7 @@ export default function SVGGrainEffect() {
       <filter id="noise">
         <feTurbulence
           type="turbulence"
-          baseFrequency="0.7"
+          baseFrequency="0.4"
           numOctaves="6"
           stitchTiles="stitch"
         ></feTurbulence>
