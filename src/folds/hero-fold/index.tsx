96fd4250134110967d6da8e97b6da2142ad3a031
Samshh <samdacs2@gmail.com>
2025-01-16 13:10:40 +0800

    change hero

--- src/folds/hero-fold/index.tsx
+++ src/folds/hero-fold/index.tsx
@@ -13,7 +13,6 @@ interface HeroFoldProps {
 }
 
 export default function HeroFold({ projectsRef, contactRef }: HeroFoldProps) {
-
   const textRef1 = useGradientText();
   const textRef2 = useGradientText();
 
@@ -87,17 +86,16 @@ export default function HeroFold({ projectsRef, contactRef }: HeroFoldProps) {
   useAnimateButton(spanRef3, buttonsRef3);
 
   return (
-    <div
-      className="triggerHero h-full min-h-screen px-4 py-4 flex items-center max-w-[1280px] mx-auto select-none"
-    >
+    <div className="triggerHero h-full min-h-screen px-4 py-4 flex items-center max-w-[1280px] mx-auto select-none">
       <div className="flex flex-col justify-center h-full md:w-1/2 gap-[1.5rem]">
         <div className="flex flex-col justify-center items-start">
           <h4 className="font-thin text-[#535353]" ref={introTextRef}>
             Sam Dacara
           </h4>
           <h1 ref={headlineRef}>
-            A <span ref={textRef1}>Web</span> Developer<span className="text-[#333333]">,</span> <br />
-            skilled in <span ref={textRef2}>front-end</span>
+            Crafting <span ref={textRef1}>intuitive</span>
+            <span className="text-[#333333]">,</span> <br />
+            <span ref={textRef2}>Front-end</span> solutions
             <span className="text-[#333333]">.</span>
           </h1>
         </div>
